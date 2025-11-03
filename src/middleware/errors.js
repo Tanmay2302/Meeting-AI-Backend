08b36cd7c2de24f35c571850b7b342eca6e62cbf
Tanmay Gurjar <tanmaygurjar2302@gmail.com>
2025-11-03 12:09:16 +0530

    Update errors.js

--- src/middleware/errors.js
+++ src/middleware/errors.js
@@ -1,5 +1,5 @@
 export function errorHandler(err, req, res, next) {
-  // eslint-disable-line
+
   const code = err.code && Number.isInteger(err.code) ? err.code : 500;
   res.status(code).json({
     error: err.message || "Internal Server Error",
